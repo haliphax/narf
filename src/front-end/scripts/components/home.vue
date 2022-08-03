89b2f58a9b1191b1a9e7cb51adc9d1a8f6ac7f05
haliphax <haliphax@gmail.com>
2022-08-03 12:27:46 -0500

    ✨ home component and route

---
+++ src/front-end/scripts/components/home.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+import { Component } from 'vue';
+
+const Home: Component = {
+
+};
+
+export default Home;
+</script>
+
+<template>
+	<h1>
+		narf!
+		<small>Simple story points estimation</small>
+	</h1>
+	<router-link :to="{ name: 'story', params: { story: 1 } }">Demo</router-link>
+</template>
+
+<style lang="less" scoped>
+small {
+	color: var(--color-fg-subtle);
+	display: block;
+	font-size: 50%;
+}
+</style>
--- src/front-end/scripts/router.ts
+++ src/front-end/scripts/router.ts
@@ -1,14 +1,21 @@
 import { Component } from 'vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from './components/home.vue';
 import Room from './components/room.vue';
 
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes: [
 		{
+			path: '/',
+			component: (Home as unknown) as Component,
+			name: 'home',
+		},
+		{
 			path: '/:story',
 			component: (Room as unknown) as Component,
-		}
+			name: 'story',
+		},
 	],
 });
 

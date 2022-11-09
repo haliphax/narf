0b172e462f5456f15b627b73ab0b0e62c13abb36
haliphax <haliphax@gmail.com>
2022-11-09 12:16:04 -0600

    🐛 use new method for setting base API URL

--- src/front-end/scripts/remult.ts
+++ src/front-end/scripts/remult.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { Remult } from 'remult';
 
-Remult.apiBaseUrl = 'http://localhost:3001/api'
-
 const remult = new Remult(axios);
+remult.apiClient.url = 'http://localhost:3001/api'
 
 export default remult;

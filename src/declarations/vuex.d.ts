5a13a19a111c26a661b7bb6dbfad957195a3e41c
haliphax <haliphax@users.noreply.github.com>
2023-04-30 23:41:51 -0500

    🔥 remove copypasta comment

--- src/declarations/vuex.d.ts
+++ src/declarations/vuex.d.ts
@@ -2,7 +2,6 @@ import { Store } from "vuex";
 import { storeState } from "../front-end/scripts/types";
 
 declare module "@vue/runtime-core" {
-	// Declare your own store states.
 	interface ComponentCustomProperties {
 		$store: Store<storeState>;
 	}

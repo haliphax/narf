a9c6ad1e12294fcc6e1cf9c8477c3d325cb2083d
haliphax <haliphax@gmail.com>
2023-05-08 10:51:28 -0500

    🧑‍💻 vscode workspace settings/recommendations

--- etc/install-gitmoji-hook.sh
+++ etc/install-gitmoji-hook.sh
@@ -4,9 +4,17 @@ root="$(git rev-parse --show-toplevel)"
 hook="$root/.git/hooks/prepare-commit-msg"
 cat >"$hook" <<EOF
 #!/usr/bin/env bash
+
+# check for skip var
 if [[ "\$SKIP_GITMOJI_HOOK" != "" ]]; then exit 0; fi
+
+# check for existing gitmoji message
+if [[ -f "\$1" ]] && [[ "\$2" == "message" ]] && {
+	[[ "\$(grep -E \$'^[\\u0000-\\u007f]' <\$1)" != "" ]]
+} then exit 0; fi
+
+# prompt for gitmoji
 exec < /dev/tty
 npx --package=gitmoji-cli -- gitmoji --hook \$1 \$2
 EOF
 chmod a+x "$hook"
-

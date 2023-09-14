403ec07009db2afba682b6038812cd282ee22291
haliphax <haliphax@gmail.com>
2023-09-13 23:09:43 -0500

    🔥 reduce fib scale

--- src/scales.ts
+++ src/scales.ts
@@ -1,13 +1,3 @@
-export const FIBONACCI = [
-	"0",
-	"0.5",
-	"1",
-	"2",
-	"3",
-	"5",
-	"8",
-	"13",
-	"💬",
-] as const;
+export const FIBONACCI = ["1", "2", "3", "5", "8", "13", "💬"] as const;
 
 export const TSHIRTS = ["XS", "S", "M", "L", "XL", "💬"] as const;

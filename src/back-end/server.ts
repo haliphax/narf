e8c0509214a3bc9af9b554a8e9dca8ffc9132e36
haliphax <haliphax@gmail.com>
2023-09-14 16:59:49 -0500

    🔥 remove seed data

--- src/back-end/server.ts
+++ src/back-end/server.ts
@@ -1,10 +1,8 @@
 import { UserInfo } from "remult";
 import { remultExpress } from "remult/remult-express";
 import { createKnexDataProvider } from "remult/remult-knex";
-import { v4 } from "uuid";
 import { Story } from "../models/story";
 import { Vote } from "../models/vote";
-import scales from "../scales";
 
 const server = remultExpress({
 	entities: [Story, Vote],
@@ -21,26 +19,6 @@ const server = remultExpress({
 				: undefined;
 
 		return await new Promise((resolve) => resolve(user));
-	},
-	async initApi(remult) {
-		const storyRepo = remult.repo(Story);
-
-		if ((await storyRepo.count()) === 0) {
-			const [scale, opts] = scales.entries().next().value as [string, string[]];
-
-			const story = await storyRepo.insert({
-				id: v4().replace(/[^-]/g, "0"),
-				owner: "test",
-				title: "Testing this thing",
-				scale,
-			});
-			await remult.repo(Vote).insert({
-				participantId: "test",
-				participantName: "Test Participant",
-				storyId: story.id,
-				vote: opts[0],
-			});
-		}
 	},
 });
 

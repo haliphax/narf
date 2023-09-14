cf6b94f014c6ed029fc93639793871981b74c43f
haliphax <haliphax@gmail.com>
2023-09-13 19:19:42 -0500

    🧑‍💻 combined modules.d.ts

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,6 +1,11 @@
 import { Store } from "vuex";
 import { StoreState } from "./front-end/scripts/store/types";
 
+declare module "*.vue" {
+	import Vue from "vue";
+	export default Vue;
+}
+
 declare module "@vue/runtime-core" {
 	interface ComponentCustomProperties {
 		$store: Store<StoreState>;

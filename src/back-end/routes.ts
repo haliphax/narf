7bde9c1b09dd238f93ffcaa2a0316df9313404a7
haliphax <haliphax@users.noreply.github.com>
2023-09-15 16:58:38 -0500

    👷 deploy to fly.io (#95)

--- src/back-end/routes.ts
+++ src/back-end/routes.ts
@@ -1,10 +1,8 @@
 import { Application } from "express";
 import { events } from "./routes/events";
-import glitchWebhook from "./routes/glitch-webhook";
 
 const routes = async (app: Application) => {
 	events(app);
-	glitchWebhook(app);
 };
 
 export default routes;

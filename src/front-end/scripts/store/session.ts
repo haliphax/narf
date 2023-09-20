60fcd73378d6c65d08f2763c8b04bd8f06969053
haliphax <haliphax@gmail.com>
2023-09-20 12:01:37 -0500

    ✨ export/import profile

--- src/front-end/scripts/store/session.ts
+++ src/front-end/scripts/store/session.ts
@@ -7,12 +7,20 @@ const SESSION_PREFIX = `${LOCALSTORAGE_GLOBAL_PREFIX}session.`;
 
 const keys = {
 	darkMode: `${SESSION_PREFIX}darkMode`,
-	sessionId: `${SESSION_PREFIX}sessionId`,
 	name: `${SESSION_PREFIX}name`,
+	sessionId: `${SESSION_PREFIX}sessionId`,
 };
 
 const session: Module<SessionState, StoreState> = {
 	mutations: {
+		"session.settings"(state, payload: SessionState) {
+			state.id = payload.id;
+			state.name = payload.name;
+			state.settings = payload.settings;
+			localStorage.setItem(keys.darkMode, payload.settings.darkMode.toString());
+			localStorage.setItem(keys.sessionId, payload.id);
+			localStorage.setItem(keys.name, payload.name.toString());
+		},
 		"session.settings.darkMode"(state, payload: boolean) {
 			state.settings.darkMode = payload;
 			localStorage.setItem(keys.darkMode, payload.toString());

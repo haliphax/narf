1385552c1260e2be9e207a8d2f190754f1ce6295
haliphax <haliphax@users.noreply.github.com>
2023-09-20 12:03:13 -0500

    ✨ export/import profile (#98)

--- src/front-end/scripts/components/profile.vue
+++ src/front-end/scripts/components/profile.vue
@@ -1,5 +1,6 @@
 <script lang="ts">
 import { defineComponent } from "vue";
+import { SessionState } from "../store/types";
 
 const Profile = defineComponent({
 	data() {
@@ -14,9 +15,42 @@ const Profile = defineComponent({
 		},
 	},
 	methods: {
+		exportSession() {
+			const link = document.createElement("a");
+			const content = JSON.stringify(this.$store.state.session);
+
+			link.href = `data:application/json,${content}`;
+			link.download = "narf.json";
+			link.click();
+		},
+		importSession() {
+			console.log(`start ${Date.now()}`);
+			(this.$refs.file as HTMLInputElement).click();
+		},
+		async loadFile() {
+			const files = (this.$refs.file as HTMLInputElement).files;
+
+			if (!files || files.length === 0) return;
+			if (files.length > 1) return alert("Please select a single file");
+
+			const file = files[0];
+
+			if (!file.name.endsWith(".json"))
+				return alert("Please select a JSON file");
+
+			const session: SessionState = JSON.parse(await file.text());
+
+			this.$store.commit("session.settings", session);
+			this.name = session.name;
+			requestAnimationFrame(() => {
+				this.upToDate = true;
+				alert("Profile imported successfully");
+			});
+		},
 		submit() {
 			this.$store.commit("session.settings.name", this.name);
 			this.upToDate = true;
+			alert("Profile updated");
 		},
 	},
 });
@@ -25,25 +59,55 @@ export default Profile;
 </script>
 
 <template>
-	<form @submit.prevent="submit()">
-		<fieldset class="grid">
-			<legend>Profile</legend>
-			<span>
-				<label for="name">Name:</label>
-				<input id="name" v-model="name" type="text" required />
-			</span>
-			<span>
-				<button type="submit" :disabled="upToDate">Update</button>
-			</span>
-		</fieldset>
-	</form>
+	<div>
+		<form @submit.prevent="submit()">
+			<fieldset class="grid">
+				<legend>Profile</legend>
+				<span>
+					<label for="name">Name:</label>
+					<input id="name" v-model="name" type="text" required />
+				</span>
+				<span>
+					<button type="submit" :disabled="upToDate">✅ Update</button>
+				</span>
+			</fieldset>
+		</form>
+		<ul class="unstyled grid">
+			<li>
+				<button @click="exportSession()">💾 Export</button>
+			</li>
+			<li>
+				<button @click="importSession()">📥 Import</button>
+				<input
+					ref="file"
+					accept="application/json"
+					class="no"
+					multiple
+					type="file"
+					@change="loadFile()"
+				/>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <style lang="less" scoped>
 @import "../../styles/breakpoints.less";
 
+ul.unstyled {
+	margin-top: var(--space-l);
+}
+
+ul li {
+	grid-area: 1 / span 2;
+}
+
+li button {
+	width: 100%;
+}
+
 @media @breakpoint_m {
-	.grid > span {
+	fieldset.grid > span {
 		grid-area: 1 / span 2;
 	}
 }

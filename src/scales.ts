6a8e99f99146e5854619bfe02b89781ad2e8a1c7
haliphax <haliphax@users.noreply.github.com>
2023-09-20 12:51:04 -0500

    ✨ add ❌ option to voting scales (#100)

--- src/scales.ts
+++ src/scales.ts
@@ -1,6 +1,6 @@
 const scales: Readonly<Map<string, string[]>> = new Map([
-	["Fibonacci", ["1", "2", "3", "5", "8", "13", "💬"]],
-	["T-shirt sizes", ["XS", "S", "M", "L", "XL", "💬"]],
+	["Fibonacci", ["1", "2", "3", "5", "8", "13", "💬", "❌"]],
+	["T-shirt sizes", ["XS", "S", "M", "L", "XL", "💬", "❌"]],
 ]);
 
 export default scales;

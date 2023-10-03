656f33c9ac92ec43063d7520a14fd7f517aec316
haliphax <haliphax@gmail.com>
2023-10-03 13:46:25 -0500

    ✅ unit testing prep, initial tests

--- src/front-end/scripts/constants.ts
+++ src/front-end/scripts/constants.ts
@@ -2,5 +2,8 @@ import pkg from "../../../package.json";
 
 export const LOCALSTORAGE_GLOBAL_PREFIX = "narf.";
 export const NARF_VERSION = pkg.version;
-export const ROOT_URI = (document.getElementById("rootURI") as HTMLInputElement)
-	.value;
+
+const rootUriElement = document.getElementById("rootURI");
+export const ROOT_URI = rootUriElement
+	? (rootUriElement as HTMLInputElement).value
+	: "/";
---
+++ src/front-end/test/front-end/views/home.test.ts
@@ -0,0 +1,20 @@
+import { VueWrapper, shallowMount } from "@vue/test-utils";
+import { beforeEach, describe, it } from "vitest";
+import Home from "../../../../front-end/scripts/views/home.vue";
+import store from "../../../scripts/store";
+
+let home: VueWrapper;
+
+describe("Home view", () => {
+	beforeEach(() => {
+		home = shallowMount(Home, { global: { plugins: [store] } });
+	});
+
+	it("has Profile component", ({ expect }) => {
+		expect(home.findComponent("PROFILE-STUB").exists()).toBe(true);
+	});
+
+	it("has New Story component", ({ expect }) => {
+		expect(home.findComponent("NEW-STORY-STUB").exists()).toBe(true);
+	});
+});

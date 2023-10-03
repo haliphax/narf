2b7f93997ead3e04b9168b3d6e8ae0446e22f953
haliphax <haliphax@gmail.com>
2023-10-03 18:15:46 -0500

    🩹 fix transform task, add to scripts

--- src/tasks/transform.ts
+++ src/tasks/transform.ts
@@ -3,7 +3,7 @@ import { promisify } from "util";
 
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
-const filename = "dist/front-end/index.html";
+const filename = "dist/index.html";
 const rgx = new RegExp('="/([^"]*)"', "g");
 const rootURI = process.env.ROOT_URI || "/";
 

7e67555a540f7c74faa8e6475b6a54829781cd35
haliphax <haliphax@gmail.com>
2024-11-13 08:58:55 -0600

    🔨 move vite build options into config file

--- src/front-end/vite.config.ts
+++ src/front-end/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
 	build: {
+		emptyOutDir: true,
+		outDir: "../../dist",
 		rollupOptions: {
 			plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
 		},

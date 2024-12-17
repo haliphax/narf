3929fd4b0300902fd15a2c1b968caba5d54d52e9
haliphax <haliphax@gmail.com>
2024-12-17 09:09:05 -0600

    🔥 remove unused NARF_VERSION constant

--- src/front-end/app/constants.ts
+++ src/front-end/app/constants.ts
@@ -1,7 +1,4 @@
-import pkg from "../../../package.json";
-
 export const LOCALSTORAGE_GLOBAL_PREFIX = "narf.";
-export const NARF_VERSION = pkg.version;
 
 const rootUriElement = document.getElementById("rootURI");
 export const ROOT_URI = rootUriElement

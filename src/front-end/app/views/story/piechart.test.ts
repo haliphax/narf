6060f9133b29231c04e62f8fc6dd6af91ee18a82
haliphax <haliphax@gmail.com>
2025-01-10 13:17:53 -0600

    ✅ piechart component tests

---
+++ src/front-end/app/views/story/piechart.test.ts
@@ -0,0 +1,30 @@
+import { mount, VueWrapper } from "@vue/test-utils";
+import { afterEach, beforeEach, describe, it } from "vitest";
+import PieChart from "./piechart.vue";
+
+describe("PieChart", () => {
+	let pieChart: VueWrapper;
+	const data = new Map<string, number>([
+		["1", 3],
+		["3", 1],
+		["☕", 2],
+		["❌", 1],
+		["2", 1],
+	]);
+
+	beforeEach(() => {
+		pieChart = mount(PieChart, { props: { data } });
+	});
+
+	afterEach(() => {
+		pieChart.unmount();
+	});
+
+	it("provides a slice for each option in data", ({ expect }) => {
+		const slices = pieChart
+			.findAll("div")
+			.filter((v) => v.classes().includes("🍕"));
+
+		expect(slices.length).toBe(data.size);
+	});
+});

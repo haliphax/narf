f3e2ab6574886a29625524411d674792fac24876
haliphax <haliphax@gmail.com>
2025-01-10 13:05:52 -0600

    ✅ participants component tests

---
+++ src/front-end/app/views/story/participants.test.ts
@@ -0,0 +1,81 @@
+import store from "@/front-end/app/store";
+import { mount, VueWrapper } from "@vue/test-utils";
+import { afterEach, beforeEach, describe, it } from "vitest";
+import Participants from "./participants.vue";
+
+describe("Participants", () => {
+	let participants: VueWrapper;
+	const mockVotes = [
+		{
+			participantId: "test",
+			participantName: "Test 1",
+			vote: "1",
+		},
+		{
+			participantId: "test2",
+			participantName: "Test 2",
+			vote: "2",
+		},
+	];
+
+	beforeEach(() => {
+		store.registerModule("session", {
+			state() {
+				return { id: "test" };
+			},
+		});
+
+		store.registerModule("story", {
+			mutations: {
+				story(state, payload) {
+					(state as { story: unknown }).story = payload;
+				},
+			},
+			state() {
+				return {
+					story: { votes: mockVotes },
+				};
+			},
+		});
+
+		participants = mount(Participants, { global: { plugins: [store] } });
+	});
+
+	afterEach(() => {
+		store.unregisterModule("story");
+		participants.unmount();
+	});
+
+	it("lists participants", ({ expect }) => {
+		const votes = participants.findAll("li");
+
+		expect(votes.length).toBe(mockVotes.length);
+	});
+
+	it("shows icon when vote is missing", async ({ expect }) => {
+		store.commit("story", {
+			votes: [...mockVotes, { participantName: "Test 3" }],
+		});
+		await participants.vm.$nextTick();
+
+		const voteSpans = participants
+			.findAll("span")
+			.filter(
+				(v) =>
+					v.attributes().title === "has not voted" &&
+					v.text().trim().includes("⏱️"),
+			);
+		expect(voteSpans.length).toBe(1);
+	});
+
+	it("clears span titles on reveal", async ({ expect }) => {
+		store.commit("story", { revealed: true, votes: mockVotes });
+		await participants.vm.$nextTick();
+
+		const hasTitle = participants
+			.findAll("span")
+			.filter((v) => v.attributes().title);
+
+		expect(hasTitle.length).toBe(0);
+	});
+});

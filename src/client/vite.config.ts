04687c7c377e4786b1dc2bde527f1ac92ba66da4
haliphax <haliphax@gmail.com>
2025-01-21 12:16:01 -0600

    🏷️ coerce Buffer rollup plugin type

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -2,13 +2,14 @@ import inject from "@rollup/plugin-inject";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { InputPluginOption } from "vite/node_modules/rollup";
 
 export default defineConfig({
 	build: {
 		emptyOutDir: true,
 		outDir: "../../dist",
 		rollupOptions: {
-			plugins: [inject({ Buffer: ["buffer", "Buffer"] })],
+			plugins: [inject({ Buffer: ["buffer", "Buffer"] }) as InputPluginOption],
 		},
 	},
 	plugins: [vue()],

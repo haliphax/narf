5a74d97a61749325550d80c973c8ff1112306898
haliphax <haliphax@gmail.com>
2025-04-15 12:57:18 -0500

    ⚡️ minify html

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -2,6 +2,7 @@ import inject from "@rollup/plugin-inject";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 import { InputPluginOption } from "vite/node_modules/rollup";
 
 export default defineConfig({
@@ -12,7 +13,7 @@ export default defineConfig({
 			plugins: [inject({ Buffer: ["buffer", "Buffer"] }) as InputPluginOption],
 		},
 	},
-	plugins: [vue()],
+	plugins: [createHtmlPlugin({ minify: true }), vue()],
 	resolve: {
 		alias: {
 			"@": resolve(__dirname, ".."),
